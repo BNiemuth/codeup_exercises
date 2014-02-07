a89adf44b3f67cd7c538b12e60a4915738355e80
Barry Niemuth <bn3737@yahoo.com>
2014-02-07 13:16:15 -0600

    Number guess game
    Initial version

---
+++ php/highlow.php
@@ -0,0 +1,34 @@
+<?php
+
+//Guess a number between 1 & 100.
+
+$rand = rand(1, 100);
+
+//Ask for user's input.
+
+do {
+
+	fwrite(STDOUT, 'Guess a number 1-100 ');
+
+	$guess = fgets(STDIN);
+
+//If user's guess is less than the number, output "HIGHER".
+	
+
+	if ($guess == 37) {fwrite(STDOUT, "PREDICTABLE TRY AGAIN \n");}
+
+	elseif ($guess < $rand) {fwrite(STDOUT, "HIGHER \n");}
+
+//If user's guess is greater than that the number, output 
+//	"LOWER".
+
+	elseif ($guess > $rand) {fwrite(STDOUT, "LOWER \n");}
+
+	elseif ($guess == $rand) {fwrite(STDOUT, "WELL DONE! \n");}
+
+
+} while ($guess != $rand);
+
+//If user guesses the correct number, output "WELL DONE!"
+
+?>
