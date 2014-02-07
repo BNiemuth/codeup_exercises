7f2a96b6cc5c99c2e09f1894afc535212401d930
Barry Niemuth <bn3737@yahoo.com>
2014-02-07 15:58:19 -0600

    Adding arguments

--- php/highlow.php
+++ php/highlow.php
@@ -1,8 +1,19 @@
 <?php
 
+if ($argc == 3) {
+
+//Get the starting number.
+
+$min = $argv[1];
+
+//Get the ending number.
+
+$max = $argv[2];
+}
+
 //Guess a number between 1 & 100.
 
-$rand = rand(1, 100);
+$rand = rand($min, $max);
 
 $tries = 0;
 
@@ -10,7 +21,7 @@ $tries = 0;
 
 do {
 
-	fwrite(STDOUT, 'Guess a number 1-100 ');
+	fwrite(STDOUT, "Guess a number $min-$max ");
 
 	$guess = fgets(STDIN);
 
@@ -18,7 +29,7 @@ do {
 	
 	$tries++;
 
-	if ($guess == 37) {fwrite(STDOUT, "PREDICTABLE TRY AGAIN \n");}
+	if ($guess == 37) {fwrite(STDOUT, "YOU REALLY THOUGHT IT WOULD BE THAT EASY? TRY AGAIN \n");}
 
 	elseif ($guess < $rand) {fwrite(STDOUT, "HIGHER \n");}
 
@@ -27,12 +38,12 @@ do {
 
 	elseif ($guess > $rand) {fwrite(STDOUT, "LOWER \n");}
 
+//If user guesses the correct number, output "WELL DONE!"
+
 	elseif ($guess == $rand) {fwrite(STDOUT, "WELL DONE! \n");}
 
 } 	while ($guess != $rand);
 
-fwrite(STDOUT, "IT TOOK YOU $tries TRIES \n");
-
-//If user guesses the correct number, output "WELL DONE!"
+	fwrite(STDOUT, "IT TOOK YOU $tries TRIES. \n\n");
 
 ?>
