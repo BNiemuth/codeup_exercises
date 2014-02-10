0b1e5d38c270535937989ca389869ada06d681cb
Barry Niemuth <bn3737@yahoo.com>
2014-02-10 14:48:40 -0600

    Completed Step 2

--- php/break_continue.php
+++ php/break_continue.php
@@ -1,10 +1,13 @@
 <?
 
 for ($i = 1; $i <= 100; $i++) {
-	//echo $i . "\n";
-	if ($i % 2 == 0) {
+	
+	//if ($i % 2 == 0) {
+	//continue;
+	//}
 	echo $i . "\n";
-	continue;
-	}
 
+	if ($i == 10) {
+		break;
+	}
 }
