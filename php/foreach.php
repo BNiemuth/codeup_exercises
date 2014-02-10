9cd07d7be0968b76017b06735892011a29ef0963
Barry Niemuth <bn3737@yahoo.com>
2014-02-10 13:33:03 -0600

    Step 3
    Second version (is_scalar)

--- php/foreach.php
+++ php/foreach.php
@@ -4,24 +4,33 @@ $things = array('Sgt Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (
 
 foreach ($things as $datatype) {
 	
-	if (is_int($datatype)) {
-		echo "{$datatype} is an integer\n";
+// 	if (is_int($datatype)) {
+// 		echo "{$datatype} is an integer\n";
 
-	}elseif (is_float($datatype)) {
-		echo "{$datatype} is a float\n";
+// 	}elseif (is_float($datatype)) {
+// 		echo "{$datatype} is a float\n";
 
-	}elseif (is_bool($datatype)) {
-		echo "{$datatype} is a boolean\n";
+// 	}elseif (is_bool($datatype)) {
+// 		echo "{$datatype} is a boolean\n";
 
-	}elseif (is_array($datatype)) {
-		echo "datatype is an array\n";
+// 	}elseif (is_array($datatype)) {
+// 		echo "datatype is an array\n";
 
-	}elseif (is_null($datatype)) {
-		echo "{$datatype} is null\n";
+// 	}elseif (is_null($datatype)) {
+// 		echo "{$datatype} is null\n";
 
-	}elseif (is_string($datatype)) {	
-		echo "{$datatype} is a string\n";
+// 	}elseif (is_string($datatype)) {	
+// 		echo "{$datatype} is a string\n";
 
-}
-}
+// }
+
+	if (is_scalar($datatype)) {
+		echo "{$datatype} \n";
+
+	}
+
+		else {
+			echo "\n";
+			 }
+	}		 
 ?>
