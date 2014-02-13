b35e8fdcaf20bea4dc135223a248541784e1bfe3
Barry Niemuth <bn3737@yahoo.com>
2014-02-13 16:04:31 -0600

    Update with function

--- php/list_with _commas.php
+++ php/list_with _commas.php
@@ -3,6 +3,9 @@
 // Converts array into list n1, n2, ..., and n3
 function humanized_list($input) {
   // My awesome solution goes here!
+$exploded = explode(',',$input);
+$last = array_pop($exploded);
+return implode(',', $exploded) . ", and $last";
 }
 
 // List of famous peeps
@@ -10,19 +13,17 @@ $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mal
 
 $physicists_array = explode(', ', $physicists_string);
 
+//$last_item = array_pop($physicists_array);
 
-$last_item = array_pop($physicists_array);
+//array_push($physicists_array, "and $last_item");
 
-
-array_push($physicists_array, "and $last_item");
-
-$famous_fake_physicists=implode(', ', $physicists_array).PHP_EOL;
+//$famous_fake_physicists=implode(', ', $physicists_array).PHP_EOL;
 
 //Humanize that list
 $famous_fake_physicists = humanized_list($physicists_string);
 
 // Output sentence
-echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";echo PHP_EOL;
 
 
 ?>
